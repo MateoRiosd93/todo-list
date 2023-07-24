fd49f35d30d886940d81ee0eef09b0baa92e6b71
Mateo Rios Diaz <mateorios993@gmail.com>
2023-07-24 08:08:48 -0500

    Se crean metodos para toggleTodo, y listar los todos completos e incompletos

--- src/store/todoState.ts
+++ src/store/todoState.ts
@@ -10,7 +10,10 @@ export interface Todo {
 interface TodoState {
     todos: Todo[]
     addTodo: (todo: Todo) => void
-    removeTodo: (id: string) => void 
+    removeTodo: (id: string) => void
+    toggleTodo: (id: string) => void
+    completedTodos: () => void
+    incompletedTodos: () => void
 }
 
 export const useTodoState = create<TodoState>((set) => ({
@@ -20,5 +23,27 @@ export const useTodoState = create<TodoState>((set) => ({
     })),
     removeTodo: (id: string) => set(state => ({
         todos: state.todos.filter(todo => todo.id !== id)
+    })),
+    toggleTodo: (id: string) => set(state => ({
+        todos: toggleTodos(state, id)
+    })),
+    completedTodos: () => set(state => ({
+        todos: state.todos.filter(todo => todo.completed)
+    })),
+    incompletedTodos: () => set(state => ({
+        todos: state.todos.filter(todo => !todo.completed)
     }))
 }))
+
+const toggleTodos = (state: TodoState, id: string) => {
+    return state.todos.map(todo => {
+        if (todo.id === id) {
+            return {
+                ...todo,
+                completed: !todo.completed
+            }
+        } else {
+            return todo
+        }
+    })
+}
