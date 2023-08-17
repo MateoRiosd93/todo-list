2be732552fe630929943759f918ceaf1673a1350
Mateo Rios Diaz <mateorios993@gmail.com>
2023-08-16 19:52:10 -0500

    Se implementa persintencia de los todos con el localStorage

--- src/store/todoState.ts
+++ src/store/todoState.ts
@@ -13,36 +13,62 @@ interface TodoState {
     addTodo: (todo: Todo) => void
     removeTodo: (id: string) => void
     toggleTodo: (id: string) => void
-    completedTodos: () => void
-    pendingTodos: () => void
+    completedTodo: () => void
+    pendingTodo: () => void
     allTodos: () => void
 }
 
+const getInitialStateTodos = () => JSON.parse(localStorage.getItem('TODOS') as string) ?? []
+
 export const useTodoState = create<TodoState>((set) => ({
-    todos: [],
+    todos: getInitialStateTodos() ?? [],
     filteredTodos: [],
-    addTodo: (todo: Todo) => set((state) => ({
-        todos: [...state.todos, todo],
-        filteredTodos: []
-    })),
-    removeTodo: (id: string) => set((state) => ({
-        todos: state.todos.filter(todo => todo.id !== id),
-        filteredTodos: state.filteredTodos.filter(todo => todo.id !== id),
-    })),
-    toggleTodo: (id: string) => set(state => ({
-        todos: toggleTodos(state.todos, id),
-        filteredTodos: toggleTodos(state.filteredTodos, id)
-    })),
-    completedTodos: () => set(state => ({
+    addTodo: (todo: Todo) => set((state) => saveTodo(state, todo)),
+    removeTodo: (id: string) => set((state) => deleteTodo(state, id)),
+    toggleTodo: (id: string) => set(state => toggleTodos(state, id)),
+    completedTodo: () => set(state => ({
         filteredTodos: state.todos.filter(todo => todo.completed)
     })),
-    pendingTodos: () => set(state => ({
+    pendingTodo: () => set(state => ({
         filteredTodos: state.todos.filter(todo => !todo.completed)
     })),
     allTodos: () => set(state => ({ todos: state.todos, filteredTodos: [] }))
 }))
 
-const toggleTodos = (todos: Todo[], id: string) => {
+const saveTodo = (state: TodoState, todo: Todo) => {
+    setTodosLocalStorage([...state.todos, todo])
+    return {
+        todos: [...state.todos, todo],
+        filteredTodos: []
+    }
+}
+
+const deleteTodo = (state: TodoState, id: string) => {
+    const todos = state.todos.filter(todo => todo.id !== id)
+    const filteredTodos = state.filteredTodos.filter(todo => todo.id !== id)
+
+    setTodosLocalStorage(todos)
+
+    return {
+        todos,
+        filteredTodos
+    }
+}
+
+const toggleTodos = (state: TodoState, id: string) => {
+    const todos = toggle(state.todos, id)
+
+    setTodosLocalStorage(todos)
+
+    return {
+        todos,
+        filterTodos: toggle(state.filteredTodos, id)
+    }
+}
+
+const setTodosLocalStorage = (todos: Todo[]) => localStorage.setItem('TODOS', JSON.stringify(todos))
+
+const toggle = (todos: Todo[], id: string) => {
     return todos.map(todo => {
         if (todo.id === id) {
             return {
